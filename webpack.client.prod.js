19ccf3c9f034a5ab591d0dc58cf4d5ac58366ff6
shubham Agarwal <shubham150@gmail.com>
2020-06-09 17:16:36 +0530

    add eslint plugin

---
+++ webpack.client.prod.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const baseConfig = require('./webpack.base');
+// const { InjectManifest } = require('workbox-webpack-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const config = {
+  mode: 'production',
+  stats: {
+    colors: false,
+    hash: true,
+    timings: true,
+    assets: true,
+    chunks: true,
+    chunkModules: true,
+    modules: true,
+    children: true,
+  },
+  optimization: {
+    minimizer: [
+      // we specify a custom UglifyJsPlugin here to get source maps in production
+      new UglifyJSPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true,
+        },
+        sourceMap: false,
+      }),
+    ],
+  },
+  // Tell webpack to root file of our server app
+  entry: './src/client/client.js',
+
+  // Tell webpack where to put output file
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    // new BundleAnalyzerPlugin(),
+    new CompressionPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    // new InjectManifest({
+    //   swDest: './src/client/sw.js',
+    //   swSrc: 'service-worker.js',
+    // }),
+  ],
+};
+
+module.exports = merge(baseConfig, config);
