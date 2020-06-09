35fb46a86350fc17fec901642063a53f05e0d8a2
shubham Agarwal <shubham150@gmail.com>
2020-06-09 20:46:32 +0530

    change in component

--- src/client/components/header/index.js
+++ src/client/components/header/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { HeaderContainer } from './styled';
 
 export default function Header() {
   return (
     <HeaderContainer>
-      <Link to="/">
-        <img src="https://news.ycombinator.com/y18.gif" alt="logo" />{' '}
-        <span data-testid="site_title">Hacker News</span>
-      </Link>
+      <span data-testid="site_title">Hacker News</span>
     </HeaderContainer>
   );
 }
